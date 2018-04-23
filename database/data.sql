7368b2235adaa3626bd24af69573173f32cb1862
Aaron Tyler <aaron.tyler@techelevator.com>
2018-04-23 08:45:19 -0400

    Added seed project

---
+++ database/data.sql
@@ -0,0 +1,9 @@
+-- *****************************************************************************
+-- This script contains INSERT statements for populating tables with seed data
+-- *****************************************************************************
+
+BEGIN;
+
+-- INSERT statements go here
+
+COMMIT;
---
+++ database/schema.sql
@@ -0,0 +1,9 @@
+-- *************************************************************************************************
+-- This script creates all of the database objects (tables, constraints, etc) for the database
+-- *************************************************************************************************
+
+BEGIN;
+
+-- CREATE statements go here
+
+COMMIT;
