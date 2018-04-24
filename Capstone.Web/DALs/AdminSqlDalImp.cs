14fae5c199f14a8e63c3c3d96fe14ecf4e2228b1
Greg Austrow <gregoryaustrow@gmail.com>
2018-04-24 17:30:25 -0400

    I lost track of doing a lot of stuff

--- Capstone.Web/DALs/AdminSqlDalImp.cs
+++ Capstone.Web/DALs/AdminSqlDalImp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using Capstone.Web.DALs.Interfaces;
@@ -16,32 +17,6 @@ namespace Capstone.Web.DALs
             this.connectionString = connectionString;
         }
 
-        public bool AddNewCourse(Course course)
-        {
-            string SQL_AddNewCourse = @"Insert into forum_post (username, subject, message) 
-            values (@username, @subject, @message); Select Cast(Scope_identity() as int);";
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(_connectionString))
-                {
-                    conn.Open();
-
-                    SqlCommand cmd = new SqlCommand(SQL_SaveNewPost, conn);
-
-                    cmd.Parameters.Add(new SqlParameter("@username", post.Username));
-                    cmd.Parameters.Add(new SqlParameter("@subject", post.Subject));
-                    cmd.Parameters.Add(new SqlParameter("@message", post.Message));
-                    int newId = (int)cmd.ExecuteScalar();
-                }
-            }
-
-            catch (SqlException e)
-            {
-                Console.WriteLine(e.Message);
-                isSuccessful = false;
-            }
-            return isSuccessful;
-        }
 
     }
 }
