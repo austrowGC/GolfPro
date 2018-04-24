9126c9cb1c1db5a58a5937a3cac03733ddecb234
Greg Austrow <gregoryaustrow@gmail.com>
2018-04-24 10:58:25 -0400

    login and registration views, view models

---
+++ Capstone.Web/Models/ViewModels/Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models.ViewModels
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "*")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string Password { get; set; }
+
+    }
+}
---
+++ Capstone.Web/Models/ViewModels/Registration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class Registration
+    {
+        [Required(ErrorMessage="*")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string Password { get; set; }
+
+    }
+}
