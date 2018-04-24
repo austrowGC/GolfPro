29125bb39c312c2fc32af214d12c1401b66ecd67
Dalton Limmer <dlimmer.10@gmail.com>
2018-04-24 16:24:26 -0400

    some stuff

--- Capstone.Web/DALs/AdminSqlDalImp.cs
+++ Capstone.Web/DALs/AdminSqlDalImp.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Capstone.Web.DALs.Interfaces;
+using Capstone.Web.Models;
 
 namespace Capstone.Web.DALs
 {
     public class AdminSqlDalImp : AdminSqlDal
     {
+        private readonly string connectionString;
+
+        public AdminSqlDalImp(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public bool AddNewCourse(Course course)
+        {
+            string SQL_AddNewCourse = @"Insert into forum_post (username, subject, message) 
+            values (@username, @subject, @message); Select Cast(Scope_identity() as int);";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_SaveNewPost, conn);
+
+                    cmd.Parameters.Add(new SqlParameter("@username", post.Username));
+                    cmd.Parameters.Add(new SqlParameter("@subject", post.Subject));
+                    cmd.Parameters.Add(new SqlParameter("@message", post.Message));
+                    int newId = (int)cmd.ExecuteScalar();
+                }
+            }
+
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+                isSuccessful = false;
+            }
+            return isSuccessful;
+        }
+
     }
 }
