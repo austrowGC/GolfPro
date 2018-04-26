6c7c18d44c3e76198c0bc8a6ba549f8ed570dc42
Greg Austrow <gregoryaustrow@gmail.com>
2018-04-26 11:27:46 -0400

    front-end, jQuery & bootstrap, customer stylesheet

---
+++ Capstone.Web/Scripts/jquery.golfpro.js
@@ -0,0 +1,11 @@
+﻿$(Document).ready(function(){
+    $("header, main").addClass("container")
+	$("nav").addClass("navbar navbar-expand-lg navbar-light")
+	$("nav ul").addClass("navbar-nav mr-auto list-unstyled list-inline")
+	$("nav ul li").addClass("nav-item")
+
+	$("form").parent("div").addClass("container-fluid")
+	$("form div").addClass("form-group")
+	$("form button:last-child").addClass("btn btn-primary")
+    $("span.field-validation-error").addClass("error")
+});
