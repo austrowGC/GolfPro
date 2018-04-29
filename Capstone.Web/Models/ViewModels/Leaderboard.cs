60f0e4763db9443b63f2735f2ed7b1757b0d517d
Dalton Limmer <dlimmer.10@gmail.com>
2018-04-29 18:28:41 -0400

    changed aesthetics of login, register, add course

--- Capstone.Web/Models/ViewModels/Leaderboard.cs
+++ Capstone.Web/Models/ViewModels/Leaderboard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capstone.Web.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace Capstone.Web.Models
     public class Leaderboard
     {
         public List<User> Users { get; set; }
+        public Course course { get; set; }
 
     }
 }
