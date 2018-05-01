7dd8aa1e8b891cfaa4cf98b88a8a25dfcb745518
Drew Keriazes <drewkeriazes@gmail.com>
2018-05-01 14:05:51 -0400

    frontend changes on menues, made them smaller and got rid of the wells per chris

--- Capstone.Web/Scripts/jquery.golfpro.js
+++ Capstone.Web/Scripts/jquery.golfpro.js
@@ -4,7 +4,8 @@
 	$("nav ul").addClass("navbar-nav mr-auto list-unstyled list-inline")
 	$("nav ul li").addClass("nav-item")
 
-	$("form").parent("div").addClass("container-fluid")
+    $("form").parent("div").addClass("poop-deck")
+    $("form").parent()
 	$("form").children().addClass("form-group")
 	$("form button:last-child").addClass("btn btn-primary")
     $("span.field-validation-error").addClass("error")
