7181f28b04b7510f9741e9f7d7e7cdf4aaad8475
Greg Austrow <gregoryaustrow@gmail.com>
2018-04-30 20:21:22 -0400

    new files for dashboard creation

---
+++ Capstone.Web/Models/ScoredMatch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class ScoredMatch
+    {
+        public int Holes { get; set; }
+        public int Score { get; set; }
+        public string CourseName { get; set; }
+        public string LeagueName { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Capstone.Web/Models/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<ScoredMatch> Scores { get; set; }
+        public List<League> Leagues { get; set; }
+    }
+}
